cb4855a72f0b898373fa0ffb1bf4a9353ca25d75
kenedyivan <kenedyakenaivan@gmail.com>
2018-06-16 02:41:49 +0300

    Uploading collections data

--- ecollections/urls.py
+++ ecollections/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('upload/', views.upload_file, name='upload'),
+    path('upload/collections/', views.upload_file_collections, name='upload_collections'),
     path('<int:employer_id>/', views.detail, name='detail'),
     path('<int:employer_id>/results/', views.results, name='results'),
     path('<int:employer_id>/comment/', views.comment, name='comment'),
--- ecollections/views.py
+++ ecollections/views.py
@@ -1,20 +1,53 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django import forms
+from excel_parsers.employer_status_masaka import *
+from excel_parsers.ecollection_reporting import *
+import xlrd
 
 
 def index(request):
     return render(request, 'index.html')
 
+
 def upload_file(request):
     if request.method == 'POST':
-        form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            handle_uploaded_file(request.FILES['file'])
-            return HttpResponseRedirect('/success/url/')
-    else:
-        form = UploadFileForm()
-    return render(request, 'upload.html', {'form': form})
+        # file = request.FILES['file']
+        # print("File name {}".format(file.name))  # Gives name
+        handle_uploaded_file(request.FILES['file'])
+        return HttpResponseRedirect('/ecollections/')
+
+
+def handle_uploaded_file(f):
+    with open('excel_data/upload.xlsx', 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+    db_dump()
+
+
+def db_dump():
+    wbook = xlrd.open_workbook('excel_data/upload.xlsx')
+    process_workbook(wbook)
+
+
+def upload_file_collections(request):
+    if request.method == 'POST':
+        # file = request.FILES['file']
+        # print("File name {}".format(file.name))  # Gives name
+        handle_uploaded_file_collections(request.FILES['file_collections'])
+        return HttpResponseRedirect('/ecollections/')
+
+
+def handle_uploaded_file_collections(f):
+    with open('excel_data/collections.xlsx', 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+        db_dump_collections()
+
+
+def db_dump_collections():
+    wbook = xlrd.open_workbook('excel_data/collections.xlsx')
+    process_collections_workbook(wbook)
 
 
 def detail(request, employer_id):
