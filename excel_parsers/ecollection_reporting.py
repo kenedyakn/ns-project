c1dc907ae73894215668bb66dac514348cfa5c31
kenedyivan <kenedyakenaivan@gmail.com>
2018-06-16 08:43:40 +0300

    Added system ui

--- excel_parsers/ecollection_reporting.py
+++ excel_parsers/ecollection_reporting.py
@@ -22,7 +22,7 @@ conn = mysql.connector.connect(user=database_username, password=database_passwor
 
 def process_collections_workbook(workbook):
     sheets = workbook.sheet_names()
-    process_sheets_collections(workbook, sheets)
+    return process_sheets_collections(workbook, sheets)
 
 
 def process_sheets_collections(workbook, sheets):
@@ -30,14 +30,16 @@ def process_sheets_collections(workbook, sheets):
         sheet = workbook.sheet_by_name(sheet_name)
         total_rows = sheet.nrows
         total_cols = sheet.ncols
-        process_data_collections(workbook, sheet, total_rows, total_cols)
+        return process_data_collections(workbook, sheet, total_rows, total_cols)
 
 
 def process_data_collections(workbook, sheet, total_rows, total_cols):
-    add_to_db_collections(workbook, sheet, total_rows, total_cols)
+    return add_to_db_collections(workbook, sheet, total_rows, total_cols)
 
 
 def add_to_db_collections(wbook, sheet, rows, cols):
+    no_parent_warning_list = []
+    payments_warning_list = []
     cursor = conn.cursor()
     total_rows = rows
     total_cols = cols
@@ -117,6 +119,13 @@ def add_to_db_collections(wbook, sheet, rows, cols):
 
             full_query = query + values
             cursor.execute(full_query)
+
+            # Write to payments table
+            if record_has_parent(re.escape(str(record[6]).strip())):
+                add_payment_record_collections(wbook, record, payments_warning_list)
+            else:
+                no_parent_warning_list.append(record[6])
+                # print("Record {} has no employer association".format(record[6]))
             # print(date, record[9], record[10])
             record = []
             x += 1
@@ -126,5 +135,85 @@ def add_to_db_collections(wbook, sheet, rows, cols):
         print("Something went wrong: {}".format(err))
         conn.rollback()
 
+    return {"w_payments": payments_warning_list, "w_no_parent": no_parent_warning_list,
+            "cdup":len(payments_warning_list),"cnop":len(no_parent_warning_list)}
+
+
+def record_has_parent(emp_num):
+    cursor = conn.cursor()
+    has_parent = False
+    query = 'select employer_number from ' + employers_table + ' ' \
+                                                               'where employer_number = \'' + emp_num + '\' limit 1'
+    cursor = conn.cursor()
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    cursor.close()
+    count = len(rows)
+    if count > 0:
+        has_parent = True
+    return has_parent
+
+
+def is_payment_captured_collections(wbook, emp_num, month):
+    date = 'NULL'
+    wrongValue = month
+    try:
+        workbook_datemode = wbook.datemode
+        y, m, d, hh, mm, ss = xlrd.xldate_as_tuple(wrongValue, workbook_datemode)
+        date = "{0}-{1}-{2}".format(y, m, d)
+    except TypeError as err:
+        print("No date specified")
+
+    is_available = False
+    query = 'select employer_number from ' + payments_table + ' ' \
+                                                              'where employer_number = \'' + emp_num + '\' and ' \
+                                                                                                       'date = \'' + date + '\' limit 1'
+    cursor = conn.cursor()
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    cursor.close()
+    count = len(rows)
+    print("count ", count)
+    if count > 0:
+        is_available = True
+    return is_available
+
+
+
+# Add payment record function
+def add_payment_record_collections(wbook, record, payments_warning_list):
+    wrongValue = record[8]
+    date = ''
+    try:
+        workbook_datemode = wbook.datemode
+        y, m, d, hh, mm, ss = xlrd.xldate_as_tuple(wrongValue, workbook_datemode)
+        date = "{0}-{1}-{2}".format(y, m, d)
+    except TypeError as err:
+        print("No date specified")
+    cursor = conn.cursor()
+    full_query = ''
+    try:
+        if date != '':
+            query = 'insert into ' + payments_table + ' (employer_number, amount, date) values'
+            values = '(\'' + str(record[6]).strip() + '\',\'' + re.escape(
+                str(record[11]).strip()) + '\',\'' + date + '\')'
+        else:
+            query = 'insert into ' + payments_table + ' (employer_number, amount) values'
+            values = '(\'' + str(record[6]).strip() + '\',\'' + re.escape(
+                str(record[11]).strip()) + '\')'
+        full_query = query + values
+
+        print(record[6], record[8])
+
+        if is_payment_captured_collections(wbook, record[6], record[8]):
+            payments_warning_list.append((record[6], date))
+            print("Payment for {0} : month {1} captured".format(record[6], record[8]))
+        else:
+            #cursor.execute(full_query)
+            print(full_query)
+            conn.commit()
+    except mysql.connector.Error as err:
+        print("Something went wrong: {}".format(err))
+        conn.rollback()
 
 # process_collections_workbook(wbook)
